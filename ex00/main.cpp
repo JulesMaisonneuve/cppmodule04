c3eb5c1f364f2a172b380b05e89045b89a3c509a
Jules Maisonneuve <jumaison@student.42.fr>
2022-04-28 02:35:39 +0200

    ex00

--- ex00/main.cpp
+++ ex00/main.cpp
@@ -1,10 +1,11 @@
 #include "Animal.hpp"
+#include "WrongAnimal.hpp"
 
 int main(void)
 {
 	const Animal* meta = new Animal();
 	const Animal* j = new Dog();
-	const Animal* i = new Cat();
+	const WrongAnimal* i = new WrongCat();
 
 	std::cout << j->getType() << " " << std::endl;
 	std::cout << i->getType() << " " << std::endl;
